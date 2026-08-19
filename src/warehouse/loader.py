a3312f68d89f3d94e560478626fa4db48149bbe1
Neo Khunoane <neokhunoane123@gmail.com>
2026-08-19 10:25:01 +0200

    implemented the upsert logic for DimCompetition

--- src/warehouse/loader.py
+++ src/warehouse/loader.py
@@ -1,7 +1,9 @@
 from sqlalchemy import text
 from src.warehouse.connection import engine
+from src.utils.logger import setup_logging
 import logging
 
+setup_logging()
 logger = logging.getLogger(__name__)
 
 def load_comps(competition):
@@ -26,6 +28,15 @@ def load_comps(competition):
             :has_playoff_series,
             :logo_id
         )
+        ON CONFLICT(comp_id)
+        DO UPDATE SET
+            comp_name = :comp_name,
+            country = :country,
+            tier = :tier,
+            has_rounds = :has_rounds,
+            has_groups = :has_groups,
+            has_playoff_series = :has_playoff_series,
+            logo_id = :logo_id
     """)
 
     with engine.begin() as conn:
